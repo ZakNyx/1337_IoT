a195da2510c3c00ab7218b2009da286c6068b736
Zakaria IHIRRI <gobuwu@hotmail.com>
2026-05-17 17:37:11 +0100

    fix(ram) : changed ram from 512 to 1024 mo and fix the ingress file

--- p2/scripts/server.sh
+++ p2/scripts/server.sh
@@ -4,9 +4,18 @@
 sudo apt-get update -y
 sudo apt-get install -y curl
 
+sudo fallocate -l 1G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+# ----------------------
 
 # Install K3s strictly in server mode
+echo "⚙️ Installing K3s..."
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=192.168.56.110 --write-kubeconfig-mode 644" sh -
-
+ 
 # Setup alias
 echo "alias k='kubectl'" >> /home/vagrant/.bashrc
+
+echo "✅ Server setup complete!"
